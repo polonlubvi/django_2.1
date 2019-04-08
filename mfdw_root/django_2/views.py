4500024436b7cccb95ce11d04edfb987e78ad945
Ivan Dubrovskiy <ivan.dubrovskiy@gmail.com>
2019-04-08 10:55:29 +0300

    Add url dispatcher to index page

---
+++ mfdw_root/django_2/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("<h1>The Meandco Homepage</h1>")
+
+# Create your views here.
